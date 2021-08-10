39fea51131eb7abe7c864455c6ccf36c5a1390a1
djutifai <djutifai@MacBook-Air-Artem.local>
2021-08-10 20:22:23 +0300

    Revert "rebuilding the system (now client accepts signals as well as server)"
    This reverts commit 8174eefe51e8097e9e7de2eeb6e79a769ed310bc.

--- src/client.c
+++ src/client.c
@@ -1,49 +1,23 @@
 #include "../includes/minitalk.h"
 
-static void	ft_send_char(pid_t server_pid, char *str)
+void	ft_send_char(pid_t server_pid, char c)
 {
-	static int		counter;
-	static size_t	i;
-	static char		*mystr;
-	static pid_t	pid;
+	int	counter;
 
-	if (!counter)
+	counter = 1 << 6;
+	while (counter)
 	{
-		counter = 1 << 6;
-		if (!i)
-			i = 0;
+		if (c & counter)
+		{
+			kill(server_pid, SIGUSR1);
+		}
 		else
 		{
-			write(1, " ", 1);
-			write(1, &mystr[i], 1);
-			write(1, "\n", 1);
-			i++;
+			kill(server_pid, SIGUSR2);
 		}
-		if (!mystr)
-		{
-			mystr = str;
-			pid = server_pid;
-		}
-		if (!mystr[i])
-			exit(1);
+		counter >>= 1;
+		usleep(50);
 	}
-	if (mystr[i] & counter)
-	{
-		write(1, "1", 1);
-		kill(pid, SIGUSR1);
-	}
-	else
-	{
-		write(1, "0", 1);
-		kill(pid, SIGUSR2);
-	}
-	counter >>= 1;
-}
-
-static void ft_sighandler(int sig)
-{
-	(void) sig;
-	ft_send_char(0, 0);
 }
 
 int	main(int argc, char **argv)
@@ -55,14 +29,12 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 	{
 		write(1, "Check arguments!\nRight usage ./client pid message\n", 50);
-		return (0);
+		return 0;
 	}
 	server_pid = (pid_t)ft_atoi(argv[1]);
-	signal(SIGUSR1, ft_sighandler);
-	signal(SIGUSR2, ft_sighandler);
-	ft_send_char(server_pid, argv[2]);
-	while (1)
+	while (argv[2][i])
 	{
-		pause();
+		ft_send_char(server_pid, argv[2][i]);
+		i++;
 	}
 }
